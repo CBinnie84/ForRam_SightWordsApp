486dedeef52780da60156d212af7ceb82929e1b8
Christopher Binnie <50018406+CBinnie84@users.noreply.github.com>
2023-12-30 05:58:26 -0700

    Update script.js
    added sentence for blanks

--- script.js
+++ script.js
@@ -1,55 +1,80 @@
 const sightWordsFirstGrade = ["the", "and", "you", "my", "is", "it", "in", "to", "we", "go", /* Add more first-grade words here */];
 const sightWordsSecondGrade = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came", "did", "do", "eat", "four", "get", "good", "have", "he", "into", "like", "must", "new", "no", "now", "on", "our", "out", "please", "pretty", "ran", "ride", "saw", "say", "she", "so", "soon", "that", "there", "they", "this", "too", "under", "want", "was", "well", "went", "what", "white", "who", "will", "with", "yes", /* Add more second-grade words here */];
 
-const wordDisplay = document.getElementById('word-display');
-const wordInput = document.getElementById('word-input');
-const checkWordBtn = document.getElementById('check-word-btn');
-const nextWordBtn = document.getElementById('next-word-btn');
+const sentencesContainer = document.getElementById('sentences-container');
+const checkWordsBtn = document.getElementById('check-words-btn');
+const nextSentencesBtn = document.getElementById('next-sentences-btn');
 const progressDisplay = document.getElementById('progress');
+const nameInput = document.getElementById('name-input');
 
-let currentWordIndex = 0;
+let sentences = [];
+let currentSentenceIndex = 0;
 let correctAnswers = 0;
 
-function showNextWord() {
+function generateRandomSentence() {
     const allWords = [...sightWordsFirstGrade, ...sightWordsSecondGrade];
+    const sentenceTemplate = "This is ___ and we ___ to the ___ to ___.";
 
-    if (currentWordIndex < allWords.length) {
-        const currentWord = allWords[currentWordIndex];
+    // Replace placeholders with random sight words
+    const sentence = sentenceTemplate.replace(/___/g, () => {
+        const randomIndex = Math.floor(Math.random() * allWords.length);
+        return allWords[randomIndex];
+    });
 
-        wordDisplay.textContent = `Fill in the blank: "${currentWord.substring(0, 1)}____${currentWord.substring(2)}"`;
-        wordInput.value = '';
-        checkWordBtn.disabled = false;
-        nextWordBtn.disabled = true;
+    return sentence;
+}
+
+function showNextSentences() {
+    const sentence = generateRandomSentence();
+
+    sentences.push(sentence);
+
+    if (currentSentenceIndex < sentences.length) {
+        const currentSentence = sentences[currentSentenceIndex];
+
+        sentencesContainer.innerHTML = `Hello ${nameInput.value}! ${currentSentence}`;
+        checkWordsBtn.disabled = false;
+        nextSentencesBtn.disabled = true;
     } else {
-        wordDisplay.textContent = "Mission Accomplished!";
-        checkWordBtn.disabled = true;
-        nextWordBtn.disabled = true;
+        sentencesContainer.textContent = "Mission Accomplished!";
+        checkWordsBtn.disabled = true;
+        nextSentencesBtn.disabled = true;
     }
 }
 
-function checkWord() {
-    const allWords = [...sightWordsFirstGrade, ...sightWordsSecondGrade];
-    const currentWord = allWords[currentWordIndex];
-    const userAnswer = wordInput.value.toLowerCase();
+function checkWords() {
+    const currentSentence = sentences[currentSentenceIndex];
+    const blanksCount = (currentSentence.match(/___/g) || []).length;
 
-    if (userAnswer === currentWord) {
-        correctAnswers++;
-        progressDisplay.textContent = `Progress: ${Math.round((correctAnswers / allWords.length) * 100)}%`;
-        wordDisplay.textContent = `Good job! You got the word right: "${currentWord}"`;
-    } else {
-        wordDisplay.textContent = `Oops, try again. Fill in the blank: "${currentWord.substring(0, 1)}____${currentWord.substring(2)}"`;
+    for (let i = 0; i < blanksCount; i++) {
+        const currentWord = getCurrentWord();
+        const userAnswer = prompt(`Fill in the blank: "${currentWord}"`);
+
+        if (userAnswer.toLowerCase() === currentWord.toLowerCase()) {
+            correctAnswers++;
+        }
     }
 
-    checkWordBtn.disabled = true;
-    nextWordBtn.disabled = false;
+    const progress = Math.round((correctAnswers / blanksCount) * 100);
+    progressDisplay.textContent = `Progress: ${progress}%`;
+
+    const completedSentence = currentSentence.replace(/___/g, '<span class="completed-blank">____</span>');
+    sentencesContainer.innerHTML = `Good job, ${nameInput.value}! ${completedSentence}`;
+
+    checkWordsBtn.disabled = true;
+    nextSentencesBtn.disabled = false;
 }
 
-nextWordBtn.addEventListener('click', () => {
-    currentWordIndex++;
-    showNextWord();
+function getCurrentWord() {
+    const allWords = [...sightWordsFirstGrade, ...sightWordsSecondGrade];
+    return allWords[Math.floor(Math.random() * allWords.length)];
+}
+
+nextSentencesBtn.addEventListener('click', () => {
+    currentSentenceIndex++;
+    showNextSentences();
 });
 
-checkWordBtn.addEventListener('click', checkWord);
+checkWordsBtn.addEventListener('click', checkWords);
 
-showNextWord();
-
+showNextSentences();
