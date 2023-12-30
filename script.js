eb1885f48c390a5a735f74adfe99fd294f2748d4
Christopher Binnie <50018406+CBinnie84@users.noreply.github.com>
2023-12-29 22:28:56 -0700

    Update script.js
    more functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sightWordsFirstGrade = ["the", "and", "you", "my", "is", "it", "in", "to",
 const sightWordsSecondGrade = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came", "did", "do", "eat", "four", "get", "good", "have", "he", "into", "like", "must", "new", "no", "now", "on", "our", "out", "please", "pretty", "ran", "ride", "saw", "say", "she", "so", "soon", "that", "there", "they", "this", "too", "under", "want", "was", "well", "went", "what", "white", "who", "will", "with", "yes", /* Add more second-grade words here */];
 
 const wordDisplay = document.getElementById('word-display');
+const nameInput = document.getElementById('name-input');
 const wordInput = document.getElementById('word-input');
 const checkWordBtn = document.getElementById('check-word-btn');
 const nextWordBtn = document.getElementById('next-word-btn');
@@ -9,13 +10,21 @@ const progressDisplay = document.getElementById('progress');
 
 let currentWordIndex = 0;
 let correctAnswers = 0;
+let userName = "";
 
-// Function to shuffle an array using Fisher-Yates algorithm
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+function initializeGame() {
+    nameInput.addEventListener('input', () => {
+        userName = nameInput.value;
+    });
+
+    nextWordBtn.addEventListener('click', () => {
+        currentWordIndex++;
+        showNextWord();
+    });
+
+    checkWordBtn.addEventListener('click', checkWord);
+
+    showNextWord();
 }
 
 function showNextWord() {
@@ -25,12 +34,12 @@ function showNextWord() {
     if (currentWordIndex < allWords.length) {
         const currentWord = allWords[currentWordIndex];
 
-        wordDisplay.textContent = `Fill in the blank: "${currentWord.substring(0, 1)}____${currentWord.substring(2)}"`;
+        wordDisplay.textContent = `Hey ${userName}, fill in the blank: "${currentWord.substring(0, 1)}____${currentWord.substring(2)}"`;
         wordInput.value = '';
         checkWordBtn.disabled = false;
         nextWordBtn.disabled = true;
     } else {
-        wordDisplay.textContent = "Mission Accomplished!";
+        wordDisplay.textContent = `Good job, ${userName}! Mission Accomplished!`;
         checkWordBtn.disabled = true;
         nextWordBtn.disabled = true;
     }
@@ -44,18 +53,21 @@ function checkWord() {
     if (userAnswer === currentWord) {
         correctAnswers++;
         progressDisplay.textContent = `Progress: ${Math.round((correctAnswers / allWords.length) * 100)}%`;
+        wordDisplay.textContent = `Good job, ${userName}! You got the word right.`;
+    } else {
+        wordDisplay.textContent = `Oops, try again, ${userName}. Fill in the blank: "${currentWord.substring(0, 1)}____${currentWord.substring(2)}"`;
     }
 
     checkWordBtn.disabled = true;
     nextWordBtn.disabled = false;
 }
 
-nextWordBtn.addEventListener('click', () => {
-    currentWordIndex++;
-    showNextWord();
-});
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
-checkWordBtn.addEventListener('click', checkWord);
-
-showNextWord();
+initializeGame();
 
