e42e88e56bc7f4359c1f3c486f040aa149073382
Christopher Binnie <50018406+CBinnie84@users.noreply.github.com>
2023-12-30 07:44:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
 const sightWordsFirstGrade = ["the", "and", "you", "my", "is", "it", "in", "to", "we", "go", /* Add more first-grade words here */];
 const sightWordsSecondGrade = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came", "did", "do", "eat", "four", "get", "good", "have", "he", "into", "like", "must", "new", "no", "now", "on", "our", "out", "please", "pretty", "ran", "ride", "saw", "say", "she", "so", "soon", "that", "there", "they", "this", "too", "under", "want", "was", "well", "went", "what", "white", "who", "will", "with", "yes", /* Add more second-grade words here */];
 
-const sentencesContainer = document.getElementById('sentences-container');
-const checkWordsBtn = document.getElementById('check-words-btn');
-const nextSentencesBtn = document.getElementById('next-sentences-btn');
+const allSightWords = [...sightWordsFirstGrade, ...sightWordsSecondGrade];
+
+const sentences = generateSentences(allSightWords);
+
+const wordDisplay = document.getElementById('word-display');
+const wordInput = document.getElementById('word-input');
+const checkWordBtn = document.getElementById('check-word-btn');
+const nextWordBtn = document.getElementById('next-word-btn');
 const progressDisplay = document.getElementById('progress');
 const nameInput = document.getElementById('name-input');
 
-let sentences = [];
 let currentSentenceIndex = 0;
 let correctAnswers = 0;
 
-function generateRandomSentence() {
-    const allWords = [...sightWordsFirstGrade, ...sightWordsSecondGrade];
-    const sentenceTemplate = "This is ___ and we ___ to the ___ to ___.";
-
-    // Replace placeholders with random sight words
-    const sentence = sentenceTemplate.replace(/___/g, () => {
-        const randomIndex = Math.floor(Math.random() * allWords.length);
-        return allWords[randomIndex];
-    });
-
-    return sentence;
+function generateSentences(words) {
+    return words.map(word => `I have a ___ ${word} in my pocket.`);
 }
 
-function showNextSentences() {
-    const sentence = generateRandomSentence();
-
-    sentences.push(sentence);
-
+function showNextSentence() {
     if (currentSentenceIndex < sentences.length) {
         const currentSentence = sentences[currentSentenceIndex];
+        const sentenceWithBlanks = currentSentence.replace(/___/g, '<span class="blank">____</span>');
 
-        sentencesContainer.innerHTML = `Hello ${nameInput.value}! ${currentSentence}`;
-        checkWordsBtn.disabled = false;
-        nextSentencesBtn.disabled = true;
+        wordDisplay.innerHTML = `Hello ${nameInput.value}! ${sentenceWithBlanks}`;
+        wordInput.value = '';
+        checkWordBtn.disabled = false;
+        nextWordBtn.disabled = true;
     } else {
-        sentencesContainer.textContent = "Mission Accomplished!";
-        checkWordsBtn.disabled = true;
-        nextSentencesBtn.disabled = true;
+        wordDisplay.textContent = "Mission Accomplished!";
+        checkWordBtn.disabled = true;
+        nextWordBtn.disabled = true;
     }
 }
 
-function checkWords() {
+function checkWord() {
     const currentSentence = sentences[currentSentenceIndex];
     const blanksCount = (currentSentence.match(/___/g) || []).length;
 
     for (let i = 0; i < blanksCount; i++) {
         const currentWord = getCurrentWord();
-        const userAnswer = prompt(`Fill in the blank: "${currentWord}"`);
+        const userAnswer = wordInput.value.toLowerCase();
 
-        if (userAnswer.toLowerCase() === currentWord.toLowerCase()) {
+        if (userAnswer === currentWord) {
             correctAnswers++;
         }
     }
@@ -58,23 +51,27 @@ function checkWords() {
     const progress = Math.round((correctAnswers / blanksCount) * 100);
     progressDisplay.textContent = `Progress: ${progress}%`;
 
-    const completedSentence = currentSentence.replace(/___/g, '<span class="completed-blank">____</span>');
-    sentencesContainer.innerHTML = `Good job, ${nameInput.value}! ${completedSentence}`;
+    const completedSentence = currentSentence.replace(/___/g, `<span class="completed-blank">${getCurrentWord()}</span>`);
+    wordDisplay.innerHTML = `Good job, ${nameInput.value}! ${completedSentence}`;
 
-    checkWordsBtn.disabled = true;
-    nextSentencesBtn.disabled = false;
+    checkWordBtn.disabled = true;
+    nextWordBtn.disabled = false;
 }
 
 function getCurrentWord() {
-    const allWords = [...sightWordsFirstGrade, ...sightWordsSecondGrade];
-    return allWords[Math.floor(Math.random() * allWords.length)];
+    return allSightWords[Math.floor(Math.random() * allSightWords.length)];
 }
 
-nextSentencesBtn.addEventListener('click', () => {
-    currentSentenceIndex++;
-    showNextSentences();
+// Add event listener to start the game when the user clicks on "Click to Start"
+wordDisplay.addEventListener('click', () => {
+    showNextSentence();
 });
 
-checkWordsBtn.addEventListener('click', checkWords);
+nextWordBtn.addEventListener('click', () => {
+    currentSentenceIndex++;
+    showNextSentence();
+});
 
-showNextSentences();
+checkWordBtn.addEventListener('click', checkWord);
+
+showNextSentence();
